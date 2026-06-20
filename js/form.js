b747e7109927676890eb88990e520093953727cf
Lamaruggine <marceloandres.lamaruggine@telefonica.com>
2026-06-19 23:25:34 -0300

    arreglamos rextos, limpiamos imagenes

--- js/form.js
+++ js/form.js
@@ -1,47 +1,23 @@
 const form = document.getElementById("contactForm");
-const btnEnviar = document.getElementById("btnEnviar");
 
 form.addEventListener("submit", async (e) => {
-
     e.preventDefault();
-
-    btnEnviar.disabled = true;
-    btnEnviar.textContent = "Enviando...";
 
     const formData = new FormData(form);
 
-    try {
-
-        const response = await fetch(
-            "https://api.web3forms.com/submit",
-            {
-                method: "POST",
-                body: formData
-            }
-        );
-
-        const result = await response.json();
-
-        if (result.success) {
-
-            window.location.href = "page/gracias.html";
-
-        } else {
-
-            alert("No se pudo enviar el formulario. Intenta nuevamente.");
-
-            btnEnviar.disabled = false;
-            btnEnviar.textContent = "Enviar consulta";
+    const response = await fetch(
+        "https://api.web3forms.com/submit",
+        {
+            method: "POST",
+            body: formData
         }
+    );
 
-    } catch (error) {
+    const result = await response.json();
 
-        alert("Error de conexión. Intenta nuevamente.");
-
-        btnEnviar.disabled = false;
-        btnEnviar.textContent = "Enviar consulta";
-
-        console.error(error);
+    if (result.success) {
+        window.location.href = "page/gracias.html";
+    } else {
+        alert("Hubo un error al enviar el formulario.");
     }
-
 });
